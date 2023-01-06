3acdf00043af9842610d9f15af3280eaf9449fa5
Mafiyahunter <110088114+Mafiyahunter@users.noreply.github.com>
2023-01-06 23:25:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
+import phonenumbers
+from test import number
 
+from numbers import geocoder
+ch_nmber = phonenumbers.parse(number, "CH")
+print(geocoder.discription_for_number(ch_number, "en"))
+
+from phonenumbers import carrier
+service_nmber = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_number, "en"))
